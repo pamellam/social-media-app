5da544409aa66a72aa77d4eaa2863806eef085bc
pamellam <38889625+pamellam@users.noreply.github.com>
2024-03-21 12:29:27 +0200

    SignupForm

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -36,7 +36,7 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // This will be type-safe and validated.
     console.log(values);
   }
 
